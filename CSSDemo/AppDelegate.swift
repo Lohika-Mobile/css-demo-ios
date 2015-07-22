630ec67fe109f771d17a21adbec72e4034380111
Vjacheslav Volodko <killobatt@gmail.com>
2015-07-23 01:29:15 +0300

    Added style for navigation bar

--- CSSDemo/AppDelegate.swift
+++ CSSDemo/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        self.setupAppearance()
+        
+        
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
@@ -24,25 +26,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     }
 
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: - Split view
@@ -57,6 +52,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             }
         }
         return false
+    }
+    
+    // MARK: - Appearance 
+    
+    func setupAppearance() {
+        // navigation bar 
+        let navbarAppearance = UINavigationBar.appearance()
+        navbarAppearance.tintColor = UIColor(red: 115 / 255.0, green: 162 / 255.0, blue: 177 / 255.0, alpha: 1.0)
+        navbarAppearance.titleTextAttributes = [
+            NSFontAttributeName : UIFont(name: "BloggerSans-Medium", size: 22)!,
+            NSForegroundColorAttributeName : UIColor(white: 96 / 255.0, alpha: 1.0),
+        ]
+        
+        // navigation bar item
+        let navbarItemAppearance = UIBarButtonItem.appearance()
+        let attributes = [
+            NSFontAttributeName : UIFont.demo_regularFont
+        ]
+        navbarItemAppearance.setTitleTextAttributes(attributes, forState: UIControlState.Normal)
+        navbarItemAppearance.setTitleTextAttributes(attributes, forState: UIControlState.Highlighted)
+
     }
 
 }
